fc80229612194f483de291066f8496a216c3dff6
WeslleyFratini <weeslleey354@gmail.com>
2021-04-20 20:46:36 -0300

    chore: create config migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express, { request, response } from "express"
 
+import "./database"
+
 const app = express()
 
 app.get("/", (request, response) => {
   return response.send("Ola NLW 05")
 })
 
-app.post("/users", (request, response) => {
-  return response.json({ message: 'user salvo com sucesso'});
+app.post("/", (request, response) => {
+  return response.json({ message: 'user salvo com successo!'});
 })
 
 app.listen(3333, () => console.log('Server running on port 3333'))
