ecc05b022497a653b1e399cfe54702da932a9443
WeslleyFratini <weeslleey354@gmail.com>
2021-04-21 16:28:37 -0300

    chore: create service

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,23 +1,18 @@
-import { Request, Response } from "express"
-import { getCustomRepository } from "typeorm"
-import { SettingsRepository } from "../repositories/SettingsRepositories"
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepositories";
+import { SettingsService } from "../services/SettingsService";
 
-class SettingsController { 
-  async create(request, response){    
-      const { chat, username } = request.body
-    
-      const settingsRepository = getCustomRepository(SettingsRepository);
-    
-      const settings = settingsRepository.create({
-        chat,
-        username,
-      });
-    
-      await settingsRepository.save(settings);
-    
-      return response.json(settings)
+class SettingsController {
+  async create(request, response) {
+    const { chat, username } = request.body;
+
+    const settingsService = new SettingsService();
+
+    const settings = await settingsService.create({ chat, username})
+
+    return response.json(settings);
   }
 }
 
-export { SettingsController }
-
+export { SettingsController };
