e30b59cacb57fb50e7f88712ceab160503ee670c
WeslleyFratini <weeslleey354@gmail.com>
2021-04-21 19:46:59 -0300

    chore: create route ger message by id

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -2,24 +2,34 @@ import { Request, Response } from "express";
 import { MessagesService } from "../services/MessagesService";
 
 class MessagesController {
-  async create(request: Request, response: Response) {
-    const { admin_id, text, user_id } = request.body;
-    const messagesService = new MessagesService();
+    async create(request: Request, response: Response) {
+        const { admin_id, text, user_id } = request.body;
+        const messagesService = new MessagesService();
 
-    try {
-      const message = await messagesService.create({
-        admin_id,
-        text,
-        user_id,
-      });
+        try {
+            const message = await messagesService.create({
+                admin_id,
+                text,
+                user_id
+            });
 
-      return response.json(message);
-    } catch (err) {
-      return response.status(400).json({
-        message: err.message,
-      });
+            return response.json(message);
+        } catch (err) {
+            return response.status(400).json({
+                message: err.message,
+            })
+        }
     }
-  }
+
+    async showByUser(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const messagesService = new MessagesService();
+
+        const list = await messagesService.listByUser(id);
+
+        return response.json(list);
+    }
 }
 
 export { MessagesController };
