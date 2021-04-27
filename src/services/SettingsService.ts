0ba4520539ab948dec97c63dcea6609ea672e3a5
WeslleyFratini <weeslleey354@gmail.com>
2021-04-26 22:23:34 -0300

    chore: create service connection

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,31 +1,52 @@
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepositories";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+import { SettingsRepository } from "../repositories/SettingsRepository";
 
 interface ISettingsCreate {
   chat: boolean;
   username: string;
 }
-
 class SettingsService {
-  async create({ chat, username }: ISettingsCreate) {
-    const settingsRepository = getCustomRepository(SettingsRepository);
+  private settingsRepository: Repository<Setting>;
 
-    const userAlreadyExists = await settingsRepository.findOne({ 
-      username
+  constructor() {
+    this.settingsRepository = getCustomRepository(SettingsRepository);
+  }
+
+  async create({ chat, username }: ISettingsCreate) {
+    const userAlreadyExists = await this.settingsRepository.findOne({
+      username,
     });
 
-    if(userAlreadyExists){
-      throw new Error("User already exists")
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
     }
 
-    const settings = settingsRepository.create({
+    const settings = this.settingsRepository.create({
       chat,
       username,
     });
 
-    await settingsRepository.save(settings);
-
+    await this.settingsRepository.save(settings);
     return settings;
+  }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepository.findOne({
+      username,
+    });
+    return settings;
+  }
+
+  async update(username: string, chat: boolean) {
+    await this.settingsRepository // same as this.settingsRepository. update
+      .createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", {
+        username,
+      })
+      .execute();
   }
 }
 
--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,23 +1,39 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
 import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersService {
-  async create(email: string) {
-    const usersRepository = getCustomRepository(UsersRepository);
+  private usersRepository: Repository<User>;
 
-    const usersExists = await usersRepository.findOne({
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async findByEmail<User>(email: string) {
+    const userExists = await this.usersRepository.findOne({
+      email,
+    });
+    if (userExists) {
+      return userExists;
+    }
+  }
+
+  async create(email: string) {
+    //Verify if user exists
+    const userExists = await this.usersRepository.findOne({
       email,
     });
 
-    if (usersExists) {
-      return usersExists;
+    //If it exists, return user
+    if (userExists) {
+      return userExists;
     }
 
-    const user = usersRepository.create({
+    //If it doesn't exist, save it
+    const user = this.usersRepository.create({
       email,
     });
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
     return user;
   }
 }
