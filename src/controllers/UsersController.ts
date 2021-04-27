e35fd7a0ca7b2c1c30438f5282709d472345c2b1
WeslleyFratini <weeslleey354@gmail.com>
2021-04-26 22:20:04 -0300

    feature: create migration connectioN

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from "express";
-import { UsersService } from "../services/UsersService";
+import { UsersService } from "../services/UsersServices";
 
 class UsersController {
-  
   async create(request: Request, response: Response): Promise<Response> {
     const { email } = request.body;
-
     const usersService = new UsersService();
 
     const user = await usersService.create(email);
 
-    return response.json(user)
+    return response.json(user);
   }
 }
 
